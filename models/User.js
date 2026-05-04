720915903b76403d37917eb55fae3bf80375afb8
user-synax <usersynax@gmail.com>
2026-05-04 14:05:19 +0530

    Define User model with humor tracking

---
+++ models/User.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: '',
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+  humorType: {
+    type: String,
+    default: '',
+  },
+  likedMemes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meme',
+  }],
+  likedRedditIds: {
+    type: [String],
+    default: [],
+  },
+  swipeCount: {
+    type: Number,
+    default: 0,
+  },
+}, { timestamps: true });
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
