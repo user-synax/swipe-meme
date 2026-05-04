f98e4c4f2a96dd1d5e36ec94a65bee5f65021699
user-synax <usersynax@gmail.com>
2026-05-04 14:05:23 +0530

    Define Match model with Jaccard score support

---
+++ models/Match.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const MatchSchema = new mongoose.Schema({
+  userA: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  userB: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  commonLikes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meme',
+  }],
+  commonLikedIds: {
+    type: [String],
+    default: [],
+  },
+  commonMemeUrls: {
+    type: [String],
+    default: [],
+  },
+  jaccardScore: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'matched'],
+    default: 'matched',
+  },
+  chatUnlocked: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true });
+
+// Compound index on [userA, userB] unique
+MatchSchema.index({ userA: 1, userB: 1 }, { unique: true });
+
+export default mongoose.models.Match || mongoose.model('Match', MatchSchema);
